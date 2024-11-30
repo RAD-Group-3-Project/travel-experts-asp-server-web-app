1cc1c2a1b08bb13e0c5014e4fb606a0829bfc2f6
sorawt38 <st.sorawat38@gmail.com>
2024-11-29 23:06:40 -0700

    [build] add travelexpert database model

---
+++ TravelExpertMVC/Models/Supplier.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace TravelExpertMVC.Models;
+
+[Index("SupplierId", Name = "SupplierId")]
+public partial class Supplier
+{
+    [Key]
+    public int SupplierId { get; set; }
+
+    [StringLength(255)]
+    public string? SupName { get; set; }
+
+    [Column("is_active")]
+    public bool? IsActive { get; set; }
+
+    [InverseProperty("Supplier")]
+    public virtual ICollection<ProductsSupplier> ProductsSuppliers { get; set; } = new List<ProductsSupplier>();
+
+    [InverseProperty("Supplier")]
+    public virtual ICollection<SupplierContact> SupplierContacts { get; set; } = new List<SupplierContact>();
+}
---
+++ TravelExpertMVC/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace TravelExpertMVC.Models;
+
+[Keyless]
+public partial class User
+{
+    [Column("userid")]
+    public int Userid { get; set; }
+
+    [Column("user_login")]
+    [StringLength(50)]
+    [Unicode(false)]
+    public string UserLogin { get; set; } = null!;
+
+    [Column("user_password")]
+    [StringLength(50)]
+    [Unicode(false)]
+    public string UserPassword { get; set; } = null!;
+
+    [Column("is_admin")]
+    public bool IsAdmin { get; set; }
+}
