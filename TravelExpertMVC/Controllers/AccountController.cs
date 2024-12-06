a1a3de779cd4ba30c4eb9a7095a92a1f04b359b8
Sorawat (James) <st.sorawat38@gmail.com>
2024-12-06 03:20:28 -0700

    [Feature] Partial complete design (#6)
    * [feat] implement homepage
    
    * [feat] implement login
    
    * [feat] add href in navbar
    
    * [refactor] remove unused code
    
    * [feat] add viewdata
    
    * [fix] footer
    
    * [feat] add hero pic
    
    * [fix] picture path
    
    * [feat] change background color of login page

---
+++ TravelExpertMVC/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TravelExpertMVC.Controllers;
+public class AccountController : Controller
+{
+    public IActionResult Login()
+    {
+        TempData["IsCustomBg"] = true;
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Login(string username, string password)
+    {
+        if (username == "admin" && password == "password")
+        {
+            return RedirectToAction("Index", "Home");
+        }
+        else
+        {
+            ViewBag.Message = "Invalid username or password";
+            return View();
+        }
+    }
+
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    public IActionResult Logout()
+    {
+        // TODO: Implement logout here
+        return RedirectToAction("Index", "Home");
+    }
+}
--- TravelExpertMVC/Controllers/HomeController.cs
+++ TravelExpertMVC/Controllers/HomeController.cs
@@ -1,20 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using TravelExpertData.Data;
+using TravelExpertData.Models;
+using TravelExpertData.Repository;
 using TravelExpertMVC.Models;
 
 namespace TravelExpertMVC.Controllers;
 public class HomeController : Controller
 {
+    private readonly TravelExpertContext _context;
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(TravelExpertContext context, ILogger<HomeController> logger)
     {
+        _context = context;
         _logger = logger;
     }
 
     public IActionResult Index()
     {
-        return View();
+        List<Package> packages = PackagesRepository.GetPackages(_context);
+
+        // TODO: Implement the GetAgency method in the AgenciesRepository class
+        List<Agency> agencies = _context.Agencies.Include(a => a.Agents).ToList();
+
+        return View(new HomeViewModel() { Packages = packages, Agencies = agencies });
     }
 
     public IActionResult Privacy()
