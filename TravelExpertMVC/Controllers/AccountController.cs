cd8afc650920a05c26f72a864b2309c432979c1d
sorawt38 <st.sorawat38@gmail.com>
2024-12-06 02:06:40 -0700

    [feat] implement login

---
+++ TravelExpertMVC/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TravelExpertMVC.Controllers;
+public class AccountController : Controller
+{
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Login(string username, string password)
+    {
+        if (username == "admin" && password == "password")
+        {
+            return RedirectToAction("Index", "Home");
+        }
+        else
+        {
+            ViewBag.Message = "Invalid username or password";
+            return View();
+        }
+    }
+
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    public IActionResult Logout()
+    {
+        // TODO: Implement logout here
+        return RedirectToAction("Index", "Home");
+    }
+}
---
+++ TravelExpertMVC/Models/LoginViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TravelExpertMVC.Models;
+
+public class LoginViewModel
+{
+    [Required(ErrorMessage = "Please enter your username")]
+    public string Username { get; set; }
+    [Required(ErrorMessage = "Please enter your password")]
+    public string Password { get; set; }
+}
