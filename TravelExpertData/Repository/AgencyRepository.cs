3963c0b30412773b7464d7f03751a172f1d77e2a
sorawt38 <st.sorawat38@gmail.com>
2024-12-06 11:39:35 -0700

    [feat] implement Agency repository

---
+++ TravelExpertData/Repository/AgencyRepository.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TravelExpertData.Data;
+using TravelExpertData.Models;
+
+namespace TravelExpertData.Repository;
+public class AgencyRepository
+{
+    public static List<Agency> GetAgencies(TravelExpertContext dbContext)
+    {
+        return dbContext.Agencies.Include(a => a.Agents).ToList();
+    }
+}
