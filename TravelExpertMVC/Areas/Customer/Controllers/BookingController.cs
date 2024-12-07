d8eec53d8411163f279947890ac94db128938789
Sorawat (James) <st.sorawat38@gmail.com>
2024-12-07 15:32:45 -0700

    [Feature] Add 'My Account' for customer (#9)
    * [feat] Add Customer area with DashboardController and layout
    
    Created DashboardController in Customer area with Index action.
    Updated Index.cshtml to include dashboard content.
    Defined layout structure in _Layout.cshtml for Customer area.
    Set layout in _ViewStart.cshtml for Customer views.
    Added route mapping for areas in Program.cs.
    
    * [feat] add layout for customer's area
    
    * [fix] typo on booking controller
    
    * Refactor controllers and update views and layout
    
    Added new Booking, Dashboard, Profile, and Wallet controllers with Index action methods. Removed CustomerController. Updated Index.cshtml content for various views. Modified _Layout.cshtml to update navigation links and styling. Adjusted Program.cs to remove default and area route mappings, and added app.Run() call.
    
    * Update My Account heading and dynamic nav-link classes
    
    Replaced the <a> tag for "My Account" with an <h2> tag to change it from a link to a plain text heading. Updated nav-link elements for "Dashboard", "Profile", "Booking", and "Wallet" to dynamically add 'text-white active' classes based on the current controller using a conditional expression.
    
    * [fix] navigation
    
    * [feat] add dashboad menu
    
    * [fix] Home link

---
+++ TravelExpertMVC/Areas/Customer/Controllers/BookingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TravelExpertMVC.Areas.Customer.Controllers;
+
+[Area("Customer")]
+public class BookingController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
--- TravelExpertMVC/Program.cs
+++ TravelExpertMVC/Program.cs
@@ -46,6 +46,10 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
