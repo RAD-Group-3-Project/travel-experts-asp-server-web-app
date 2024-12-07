695836c294b4896f05ff3d439b9fa76d9d41b17b
sorawt38 <st.sorawat38@gmail.com>
2024-12-07 10:26:21 -0700

    [feat] add layout for customer's area

---
+++ TravelExpertMVC/Areas/Customer/Controllers/CustomerController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TravelExpertMVC.Areas.Customer.Controllers;
+public class CustomerController : Controller
+{
+    public IActionResult Dashboard()
+    {
+        return View();
+    }
+
+    public IActionResult Profile()
+    {
+        return View();
+    }
+
+    public IActionResult Wallet()
+    {
+        return View();
+    }
+
+    public IActionResult Bookings()
+    {
+        return View();
+    }
+}
