d1e7db3b99c8ebccf2ba62cf9329f14cb3c4474a
sorawt38 <st.sorawat38@gmail.com>
2024-12-07 10:14:00 -0700

    [feat] Add Customer area with DashboardController and layout
    Created DashboardController in Customer area with Index action.
    Updated Index.cshtml to include dashboard content.
    Defined layout structure in _Layout.cshtml for Customer area.
    Set layout in _ViewStart.cshtml for Customer views.
    Added route mapping for areas in Program.cs.

---
+++ TravelExpertMVC/Areas/Customer/Controllers/DashboardController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TravelExpertMVC.Areas.Customer.Controllers;
+[Area("Customer")]
+public class DashboardController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
