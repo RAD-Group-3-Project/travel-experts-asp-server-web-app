a8ceb0c2bb64e24df4811a1bc049fda66ff0105b
sorawt38 <st.sorawat38@gmail.com>
2024-12-08 14:48:32 -0700

    [refactor] remove unused controllers

--- TravelExpertMVC/Controllers/PackagesController.cs
+++ TravelExpertMVC/Controllers/PackagesController.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using TravelExpertData.Data;
 using TravelExpertData.Models;
 using TravelExpertData.Repository;
-using TravelExpertMVC.Areas.Customer.Controllers;
 using TravelExpertMVC.Models;
 
 namespace TravelExpertMVC.Controllers;
@@ -17,7 +16,6 @@ public class PackagesController : Controller
     // For DB Stufff
     TravelExpertContext _context;
 
-
     // Replaces manager classes and DI's inlandcontext
     public PackagesController(SignInManager<User> signInManager, UserManager<User> userManager, TravelExpertContext context)
     {
@@ -26,6 +24,7 @@ public class PackagesController : Controller
         _context = context;
 
     }
+
     // Generate a random booking number 
     public static string GenerateRandomBooking()
     {
@@ -41,107 +40,9 @@ public class PackagesController : Controller
         return new string(stringChars);
     }
 
-
-    // GET: PackageController
-    public ActionResult Index()
-    {
-        List<Package> packages = PackagesRepository.GetPackages(_context);
-        return View(packages);
-    }
-
-    // GET: PackageController/Details/5
-    public ActionResult Details(int id)
-    {
-        Package? package = PackagesRepository.GetPackageById(_context, id);
-        return View(package);
-    }
-
-    // GET: PackageController/Create
-    public ActionResult Create()
-    {
-        Package package = new Package();
-        return View(package);
-    }
-
-    // POST: PackageController/Create
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public ActionResult Create(Package package)
-    {
-        try
-        {
-            if (ModelState.IsValid)
-            {
-                PackagesRepository.AddPackage(_context, package);
-                return RedirectToAction(nameof(Index));
-            }
-            else
-            {
-                return View(package);
-            }
-        }
-        catch
-        {
-            return View(package);
-        }
-    }
-
-    // GET: PackageController/Edit/5
-    public ActionResult Edit(int id)
-    {
-        Package? package = PackagesRepository.GetPackageById(_context, id);
-        return View(package);
-    }
-
-    // POST: PackageController/Edit/5
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public ActionResult Edit(int id, Package package)
-    {
-        try
-        {
-            if (ModelState.IsValid)
-            {
-                PackagesRepository.UpdatePackage(_context, id, package);
-                return RedirectToAction(nameof(Index));
-            }
-            else
-            {
-                return View(package);
-            }
-        }
-        catch
-        {
-            return View(package);
-        }
-    }
-
-    // GET: PackageController/Delete/5
-    [HttpGet]
-    public ActionResult Delete(int id)
-    {
-        Package? package = PackagesRepository.GetPackageById(_context, id);
-        return View(package);
-    }
-
-    // POST: PackageController/Delete/5
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public ActionResult Delete(int id, IFormCollection collection)
-    {
-        try
-        {
-            PackagesRepository.DeletePackage(_context, id);
-            return RedirectToAction(nameof(Index));
-        }
-        catch
-        {
-            return View();
-        }
-    }
     [Authorize]
     public ActionResult ReviewBooking(int id)
-    {   
+    {
 
         Package? package = PackagesRepository.GetPackageById(_context, id);
         BookingViewModel newBooking = new BookingViewModel()
@@ -153,20 +54,20 @@ public class PackagesController : Controller
             Description = package.PkgDesc,
             BasePrice = package.PkgBasePrice,
             Travellers = 0,
-            TripType ="A",
+            TripType = "A",
 
         };
-        
+
 
         // Pass the list to the View using ViewBag
         ViewBag.List = GetTypes();
-        
+
 
         return View(newBooking);
     }
     [HttpPost]
     public async Task<IActionResult> ReviewBookingAsync(BookingViewModel newBookingViewModel)
-    {   
+    {
         string bookingNo = GenerateRandomBooking();
         var user = await userManager.GetUserAsync(User);
         if (ModelState.IsValid) // Checks our models validity
@@ -179,12 +80,12 @@ public class PackagesController : Controller
                 CustomerId = user.CustomerId,
                 TripTypeId = newBookingViewModel.TripType,
                 PackageId = newBookingViewModel.PackageID
-                
 
 
-            }; 
+
+            };
             BookingRepository.AddBooking(_context, newBooking);
-            return RedirectToAction("Index","Home");
+            return RedirectToAction("Index", "Home");
 
         }
         //Reloads the page if the model is not valid
@@ -194,11 +95,11 @@ public class PackagesController : Controller
     private List<SelectListItem> GetTypes()
     {
         // get list of genres 
-        List<TripType> types = TripTypeRepository.getAllTrips( _context);
+        List<TripType> types = TripTypeRepository.getAllTrips(_context);
         // create a select / dropdown list of genres 
         var list = new SelectList(types, "TripTypeId", "Ttname").ToList();
         // ensure that a genre is not slected first so as to see all trip types 
-        list.Insert(0, new SelectListItem { Text = "Select your trip type!", Value = "A" , Disabled = true });
+        list.Insert(0, new SelectListItem { Text = "Select your trip type!", Value = "A", Disabled = true });
         // Returns the list
         return list;
     }
