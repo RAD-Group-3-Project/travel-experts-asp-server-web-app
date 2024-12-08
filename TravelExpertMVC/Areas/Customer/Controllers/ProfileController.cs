8c633a3d7d5a423d48698d483505c9d7476ace5b
Chris <chris.dg.ferguson@gmail.com>
2024-12-08 16:23:29 -0700

    Working image uploads

--- TravelExpertMVC/Areas/Customer/Controllers/ProfileController.cs
+++ TravelExpertMVC/Areas/Customer/Controllers/ProfileController.cs
@@ -3,24 +3,29 @@ using Microsoft.AspNetCore.Mvc;
 using TravelExpertData.Data;
 using TravelExpertData.Models;
 using TravelExpertData.Repository;
+using System.IO;
+using Microsoft.Extensions.Hosting.Internal;
+
 
 namespace TravelExpertMVC.Areas.Customer.Controllers;
 [Area("Customer")]
 public class ProfileController : Controller
 {
-    // Identity object to manage the signin
+    // Identity object to manage the signin and file uploads via host
     private readonly SignInManager<User> signInManager;
     private readonly UserManager<User> userManager;
+    private readonly IWebHostEnvironment _host;
     // For DB Stufff
     TravelExpertContext _context;
 
 
     // Replaces manager classes and DI's inlandcontext
-    public ProfileController(SignInManager<User> signInManager, UserManager<User> userManager, TravelExpertContext context)
+    public ProfileController(SignInManager<User> signInManager, UserManager<User> userManager, TravelExpertContext context, IWebHostEnvironment host)
     {
         this.signInManager = signInManager;
         this.userManager = userManager;
         _context = context;
+        _host = host;
 
     }
     public async Task<IActionResult> Index()
@@ -53,5 +58,20 @@ public class ProfileController : Controller
 
         }
         return View(customer);
+    }
+    [HttpPost]
+    public async Task<IActionResult> EditCustomerAsync(TravelExpertData.Models.Customer customer , IFormFile profileImage)
+    {
+        if (profileImage != null) 
+        {
+            var user = await userManager.GetUserAsync(User);
+            int customerId = Convert.ToInt32(user.CustomerId);
+            var extension = Path.GetExtension(profileImage.FileName);
+            var filename = Path.Combine(_host.WebRootPath,"images", "profileImages", $"{customerId}{extension}");
+            profileImage.CopyTo(new FileStream(filename, FileMode.Create));
+
+            
+        }
+        return View();
     }
 }
