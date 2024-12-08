4ecbfb6fe1404ab47469ca3aba1cb027ee4ea8bc
sorawt38 <st.sorawat38@gmail.com>
2024-12-08 15:54:21 -0700

    [feat] create cart and cart item repository

---
+++ TravelExpertData/Repository/CartItemRepository.cs
@@ -0,0 +1,19 @@
+﻿using TravelExpertData.Data;
+using TravelExpertData.Models;
+
+namespace TravelExpertData.Repository;
+public class CartItemRepository
+{
+    public static void AddCartItem(TravelExpertContext dbContext, CartItem cartItem)
+    {
+        dbContext.CartItems.Add(cartItem);
+        dbContext.SaveChanges();
+    }
+
+    public static void RemoveCartItem(TravelExpertContext dbContext, int cartItemId)
+    {
+        var cartItem = dbContext.CartItems.Find(cartItemId);
+        dbContext.CartItems.Remove(cartItem);
+        dbContext.SaveChanges();
+    }
+}
---
+++ TravelExpertData/Repository/CartRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TravelExpertData.Data;
+using TravelExpertData.Models;
+
+namespace TravelExpertData.Repository;
+public class CartRepository
+{
+    public static Cart GetCart(TravelExpertContext dbContext, int customerId)
+    {
+        return dbContext.Carts
+            .Include(c => c.CartItems)
+            .ThenInclude(ci => ci.Package)
+            .FirstOrDefault(c => c.CustomerId == customerId);
+    }
+
+    public static void AddCart(TravelExpertContext dbContext, Cart cart)
+    {
+        dbContext.Carts.Add(cart);
+        dbContext.SaveChanges();
+    }
+
+    public static void RemoveCart(TravelExpertContext dbContext, int cartId)
+    {
+        var cart = dbContext.Carts.Find(cartId);
+        dbContext.Carts.Remove(cart);
+        dbContext.SaveChanges();
+    }
+}
