746ce0abfbfa8128663291a3963a36b422ae15bd
sorawt38 <st.sorawat38@gmail.com>
2024-12-09 18:15:07 -0700

    [feat] handle error

---
+++ TravelExpertMVC/Util/ErrorMessages.cs
@@ -0,0 +1,9 @@
+﻿namespace TravelExpertMVC.Util;
+
+public static class ErrorMessages
+{
+    public const string GENERIC = "An error occurred while processing your request. Please try again later.";
+
+    public const string INSUFFICIENT_FUND =
+        "Insufficient funds in your wallet. Please add more money to your wallet or use a different payment method.";
+}
