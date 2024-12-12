0205f65997aa83060a15b6b84603b5fb9d054211
sorawt38 <st.sorawat38@gmail.com>
2024-12-12 15:03:28 -0700

    Refactor PackagesController and update auth checks
    Refactored PackagesController to remove SignInManager<User> dependency and streamline the constructor. Updated ReviewBooking and ThankYou methods to include authentication checks. Enhanced PaymentAsync to handle missing wallet cases and refined email logic. Set cart status to Cancelled if empty. Updated ErrorMessages class and made minor cleanups including removal of unused variables and comments, and added authorization checks where necessary.

--- TravelExpertMVC/Util/ErrorMessages.cs
+++ TravelExpertMVC/Util/ErrorMessages.cs
@@ -2,10 +2,14 @@
 
 public static class ErrorMessages
 {
+    // general
     public const string GENERIC = "An error occurred while processing your request. Please try again later.";
 
+    // wallet
+    public const string WALLET_NOT_FOUND = "Can't find wallet. Please contact support.";
     public const string INSUFFICIENT_FUND =
         "Insufficient funds in your wallet. Please add more money to your wallet or use a different payment method.";
-    public const string WALLET_NOT_FOUND = "Can't find wallet. Please contact support.";
+
+    // email
     public const string EMAIL_SENDING_FAILED = "Failed to send email. Please try again later.";
 }
